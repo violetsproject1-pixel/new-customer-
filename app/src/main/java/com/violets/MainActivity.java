be51e4d9b13d85e118b837ace9012a5e53165c7d
violetsproject1-pixel <violetsproject1@gmail.com>
2026-09-10 13:06:53 +0700

    Fix formatting and spacing in MainActivity.java

--- app/src/main/java/com/violets/MainActivity.java
+++ app/src/main/java/com/violets/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView=findViewById(R.id.webview);
+        webView = findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setAllowFileAccess(true);
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean shouldOverrideUrlLoading(WebView view, String url){
                 if(url.startsWith("https://wa.me") || url.startsWith("https://api.whatsapp.com") || url.startsWith("whatsapp://")){
                     try{
-                        Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                         startActivity(intent);
                     }catch(Exception e){
                         view.loadUrl(url);
